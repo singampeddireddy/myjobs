caf41df4266c5fa2ba7155bfdb38e63a99138aad
singampeddireddy <singampeddireddy04@gmail.com>
2025-07-24 18:12:55 +0530

    done

--- backend/jobportal/urls.py
+++ backend/jobportal/urls.py
@@ -3,8 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from core.views import JobListView
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Welcome to My Job Portal!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
+    path('', home),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
